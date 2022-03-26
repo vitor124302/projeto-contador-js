b2a5abb427aa56f32b9729af3b606560f5df1ec8
vitor124302 <101463991+vitor124302@users.noreply.github.com>
2022-03-26 12:21:16 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,100 @@
+//Meu Primeiro Programa
+
+/*este é o meu Primeiro
+Bloco
+de
+Código*/
+
+console.log("hello world");
+console.log("hel lo wor ld")
+
+
+
+
+
+
+
+
+
+
+
+var numero = 10;
+var valor = 15;
+var resultado;
+
+resultado = numero + valor;
+
+console.log(resultado);
+
+resultado= resultado - 2;
+console.log(resultado);
+
+resultado = numero + (resultado * valor);
+numero = resultado;
+resultado = resultado -2;
+
+console.log(resultado);
+
+const lucas = 10;
+
+console.log(numero+valor);
+console.log(resultado - valor);
+console.log(resultado +)
+
+
+
+/*
+Tudo em javascript é uma função, no qual se define uma ação formaçao de blocos!!
+*/
+
+function teste(){
+
+
+
+}
+//funçao de forma literal
+teste()
+    const fun2 = function (){   }
+    //armazenar em uma variável
+
+const array =[function (a,b){ return a+b}, teste, fun2]
+console.log(arrey [0](2,3))
+//armazenar em um arrey
+
+const obj ={}
+obj.falar =function ( ){return 'opaa'}
+console.log (obj.falar())
+
+
+function run (fun){
+fun()
+
+
+}
+
+
+run (function (){console.log('executando.....')})
+//passar função como pareamento
+
+function soma(a,b){
+
+InputEvent("digite")
+return function(c){
+
+    console.log (a + b + c)
+}
+
+
+}
+
+soma(2,3)(4)
+const cincomais = soma (2,3)
+cincomais (4)
+
+
+var lucas = 10 ;
+i = 10;
+
+for(var lucas; lucas <= i; lucas++){
+    console.log(i)
+}
